367b5cabe42de07b52c74124e23e2b71bbb0d94e
PromiSe#### <vicimpa@ya.ru>
2023-09-03 08:00:05 +0700

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import { createRoot } from "react-dom/client";
 createRoot(document.getElementById('root')!)
   .render(
     <>
-      <h1>Hello world!</h1>
+      <h1>Hello world 123!</h1>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 3000
   },
   preview: {
     host: '127.0.0.1',

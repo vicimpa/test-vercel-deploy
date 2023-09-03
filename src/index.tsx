26c7817626b0e7bb332e5ca4c77ae960192df82b
PromiSe#### <vicimpa@ya.ru>
2023-09-03 07:50:59 +0700

    Init commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById('root')!)
+  .render(
+    <>
+      <h1>Hello world!</h1>
+    </>
+  );
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import paths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 3000
+  },
+  plugins: [
+    react({ plugins: [] }),
+    paths()
+  ],
+});
